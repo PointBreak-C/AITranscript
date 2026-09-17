7a13ef6a76925ae961dbb71f9b4bd782614acf00
PointBreak-C <priyanshdubeystudy@gmail.com>
2026-09-17 14:23:41 +0530

    Change transcription model to meta/muse-voice-transcribe-1.0

--- app.py
+++ app.py
@@ -261,7 +261,7 @@ if audio_data_to_process:
         file_payload = (clean_filename, audio_data_to_process, mime_type)
         
         transcription = client.audio.transcriptions.create(
-            model="mistralai/voxtral-mini-3b-2507",
+            model="meta/muse-voice-transcribe-1.0",
             file=file_payload,
             response_format="json" 
         )
